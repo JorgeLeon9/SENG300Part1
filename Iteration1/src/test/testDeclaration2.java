605272498b7d8b8ae209c413ca45fcaaffef7fac
Jason <jason.dam1@ucalgary.ca>
2018-03-12 14:52:18 -0600

    Finding Only Java files (w/ a bug that needs to be fixed)

--- Iteration1/src/test/testDeclaration2.java
+++ Iteration1/src/test/testDeclaration2.java
@@ -6,11 +6,22 @@ public class testDeclaration2 {
 	public static void main(String[] args){
 	
 	File filePath = new File("C:/Users/jason/Documents/School/University of Calgary/SENG 300 iteration 1/Iteration1/src/test");
-	File[] javaFiles = filePath.listFiles();{
-    for (int i = 0; i < javaFiles.length; i++) {
-	     if (javaFiles[i].isFile()) {
-	    	 System.out.println("File " + javaFiles[i].getName());
-	    	 } //dont need the directory part
+	File[] allFiles = filePath.listFiles();{
+    for (int i = 0; i < allFiles.length; i++) {
+	     if (allFiles[i].isFile()) {
+	    	 //for (int j = 0; i < allFiles.length; j++){
+	    		 String fileToCheck = allFiles[i].getName();
+	    		 String isJava = fileToCheck.substring(fileToCheck.lastIndexOf("."));
+	    		 //to test if it is .java in the end
+	    		 System.out.println(isJava);
+	    		 //current bug: I print isjava, it is ".java" but the if statement is not true.
+	    		 if(isJava == ".java"){
+	    			 System.out.println("File " + allFiles[i].getName());
+	    		 } else {
+	    			 System.out.println("false");
+	    		 }
+	    	 //}
+	     }
 	    }
 }}
 }
