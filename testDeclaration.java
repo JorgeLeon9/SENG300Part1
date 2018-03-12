386dd9e2e259e1fa927f964a7e3853f6e20ea8d4
parkerwong134 <31747530+parkerwong134@users.noreply.github.com>
2018-03-12 14:30:21 -0600

    What we now have
    Now this should print and count all the declarations for all files within a given directory.

--- testDeclaration.java
+++ testDeclaration.java
@@ -1,5 +1,6 @@
 package test;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,7 +19,9 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 public class testDeclaration {
 
-	
+	/* The getFile method takes in a String parameter and declares a newBufferedReader and 
+	 * StringBuilder to read .java files
+	 */
 	public static String getFile(String filePath) throws FileNotFoundException, IOException{
 		BufferedReader read = new BufferedReader(new FileReader(filePath));
 		StringBuilder build = new StringBuilder();
@@ -35,30 +38,48 @@ public class testDeclaration {
 	
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String filePath = "test.java";
+		
+		//declare a specific directory and puts the names of files in a list
+		File folder = new File("/Users/parkerwong/Documents/workspace/TestAstParser/src/test");
+		File[] listOfFiles = folder.listFiles();{
+	    for (int i = 0; i < listOfFiles.length; i++) {
+		     if (listOfFiles[i].isFile()) {
+		    	 System.out.println("File " + listOfFiles[i].getName());
+		    	 } else if (listOfFiles[i].isDirectory()) {
+		    		 System.out.println("Directory " + listOfFiles[i].getName());
+		    	 }
+		    }
+	}
+		//Parses the file 
+		
+		for (int i = 0; i < listOfFiles.length; i++){
+		String filePath = listOfFiles[i].getName();
+		System.out.println(filePath);
 		ASTParser parser = ASTParser.newParser(AST.JLS8);
 		char[] fileContent = getFile(filePath).toCharArray();
 		parser.setSource(fileContent);
 		CompilationUnit unit = (CompilationUnit) parser.createAST(null);
+		
 		List<String> declarations = new ArrayList();
+		
 		unit.accept(new ASTVisitor(){
 		
-			
+			//Gets the name of each variable and adds it to a list
 			public boolean visit(VariableDeclarationFragment node){
 				SimpleName name = node.getName();
-				int lineNumber = unit.getLineNumber(name.getStartPosition());
+				//int lineNumber = unit.getLineNumber(name.getStartPosition());
 				declarations.add(name.toString());
-				
-				System.out.println(declarations);
+				//System.out.println(declarations);
 				//System.out.println("Line number: " + lineNumber);
 				return false;
 			}
 		
 		});
+		//Puts the declarations of every variable into a Hashset and counts the frequency of each one
 		Set<String> uniqueSet = new HashSet<String>(declarations);
 		for (String type : uniqueSet) {
 			System.out.println(type + " Declarations found: " + Collections.frequency(declarations, type));	
 	
 		}
-	}
+	}}
 }
